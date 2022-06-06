5750a5b921b46a103f06d6f0819e407107bfc906
Animus Septimus <aseptimu@at-r7.msk.21-school.ru>
2022-06-06 15:59:49 +0300

    erase balance added

--- tree_balance.hpp
+++ tree_balance.hpp
@@ -124,6 +124,163 @@ namespace ft
 		}
 		root->_color = _black_n;
 	}
+
+	template < typename Val >
+	Node<Val>*	tree_rebalance_for_erase(Node<Val>* const node, Node<Val>& header)
+	{
+		Node<Val> *&	root = header._parent;
+		Node<Val> *&	min_node = header._left;
+		Node<Val> *&	max_node = header._right;
+		Node<Val> *		cpy  = node;
+		Node<Val> *		tmp = 0;
+		Node<Val> *		tmp_parent = 0;
+
+		if (cpy->_left == 0) // save child (if it's alone of none)
+			tmp = cpy->_right;
+		else
+		{
+			if (cpy->_right == 0) // only one (left) child
+				tmp = cpy->_left;
+			else // two children
+			{
+				cpy = cpy->_right;
+				while (cpy->_left != 0)
+					cpy = cpy->_left;
+				tmp = cpy->_right;
+			}
+		}
+		if (cpy != node) // if two child
+		{
+			node->_left->_parent = cpy;
+			cpy->_left = node->_left;
+			if (cpy != node->_right)
+			{
+				tmp_parent = cpy->_parent;
+				if (tmp)
+					tmp->_parent = cpy->_parent;
+				cpy->_parent->_left = tmp;
+				cpy->_right = node->_right;
+				node->_right->_parent = cpy;
+			}
+			else
+				tmp_parent = cpy;
+			if (root == node)
+				root = cpy;
+			else if ( node->_parent->_left == node)
+				node->_parent->_left = cpy;
+			else
+				node->_parent->_right = cpy;
+			cpy->_parent = node->_parent;
+			ft::swap(cpy->_color, node->_color);
+			cpy = node;
+		}
+		else // if one or none child
+		{
+			tmp_parent = cpy->_parent;
+			if (tmp)
+				tmp->_parent = cpy->_parent;
+			if (root == node)
+				root = tmp;
+			else
+			{
+				if (node->_parent->_left == node)
+					node->_parent->_left = tmp;
+				else
+					node->_parent->_right = tmp;
+			}
+			if (min_node == node)
+			{
+				if (node->_right == 0)
+					min_node = node->_parent;
+				else
+					min_node = Node<Val>::_Node_min(tmp);
+			}
+			if (max_node == node)
+			{
+				if (node->_left == 0)
+					max_node = node->_parent;
+				else
+					max_node = Node<Val>::_Node_max(tmp);
+			}
+		}
+		if (cpy->_color != _red_n) // if delete node is black
+		{
+			while (tmp != root && (tmp == 0 || tmp->_color == _black_n))
+			{
+				if (tmp == tmp_parent->_left)
+				{
+					Node<Val>*	tmp_uncle = tmp_parent->_right;
+					if (tmp_uncle->_color == _red_n)
+					{
+						tmp_uncle->_color = _black_n;
+						tmp_parent->_color = _red_n;
+						tree_rotate_left(tmp_parent, root);
+						tmp_uncle = tmp_parent->_right;
+					}
+					if ((tmp_uncle->_left == 0 || tmp_uncle->_left->_color == _black_n) && \
+						(tmp_uncle->_right == 0 || tmp_uncle->_right->_color == _black_n))
+					{
+						tmp_uncle->_color = _red_n;
+						tmp = tmp_parent;
+						tmp_parent = tmp_parent->_parent;
+					}
+					else
+					{
+						if (tmp_uncle->_right == 0 || tmp_uncle->_right->_color == _black_n)
+						{
+							tmp_uncle->_left->_color = _black_n;
+							tmp_uncle->_color = _red_n;
+							tree_rotate_right(tmp_uncle, root);
+							tmp_uncle = tmp_parent->_right;
+						}
+						tmp_uncle->_color = tmp_parent->_color;
+						tmp_parent->_color = _black_n;
+						if (tmp_uncle->_right)
+							tmp_uncle->_right->_color = _black_n;
+						tree_rotate_left(tmp_parent, root);
+						break ;
+					}
+				}
+				else // same, but but the left are mirrored to the right
+				{
+					Node<Val>*	tmp_uncle = tmp_parent->_left;
+					if (tmp_uncle->_color == _red_n)
+					{
+						tmp_uncle->_color = _black_n;
+						tmp_parent->_color = _red_n;
+						tree_rotate_right(tmp_parent, root);
+						tmp_uncle = tmp_parent->_left;
+					}
+					if ((tmp_uncle->_right == 0 || tmp_uncle->_right->_color == _black_n) && \
+						(tmp_uncle->_left == 0 || tmp_uncle->_left->_color == _black_n))
+					{
+						tmp_uncle->_color = _red_n;
+						tmp = tmp_parent;
+						tmp_parent = tmp_parent->_parent;
+					}
+					else
+					{
+						if (tmp_uncle->_left == 0 || tmp_uncle->_left->_color == _black_n)
+						{
+							tmp_uncle->_right->_color = _black_n;
+							tmp_uncle->_color = _red_n;
+							tree_rotate_left(tmp_uncle, root);
+							tmp_uncle = tmp_parent->_left;
+						}
+						tmp_uncle->_color = tmp_parent->_color;
+						tmp_parent->_color = _black_n;
+						if (tmp_uncle->_left)
+							tmp_uncle->_left->_color = _black_n;
+						tree_rotate_right(tmp_parent, root);
+						break ;
+					}
+				}
+			}
+			if (tmp)
+			tmp->_color = _black_n;
+		}
+		return (cpy);
+	}
 } // namespace ft
 
 #endif

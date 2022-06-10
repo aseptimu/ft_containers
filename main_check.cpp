2716247d45476232a86e3a66dc94ecc917cd1421
Aseptimu <aseptimu@student.21-school.ru>
2022-06-10 22:34:40 +0300

    some tests added

--- main_check.cpp
+++ main_check.cpp
@@ -363,31 +363,119 @@ int main(void)
 	// ERASE
 	{
 		std::cout << std::endl;
+		ft::vector<int> myvector;
+
+		// set some values (from 1 to 10)
+		for (int i=1; i<=10; i++) myvector.push_back(i);
+
+		// erase the 6th element
+		myvector.erase (myvector.begin()+5);
+
+		// erase the first 3 elements:
+		myvector.erase (myvector.begin(),myvector.begin()+3);
+
+		std::cout << "myvector contains:";
+		for (unsigned i=0; i<myvector.size(); ++i)
+			std::cout << ' ' << myvector[i];
+		std::cout << '\n';
 	}
 
 	// SWAP
 	{
 		std::cout << std::endl;
+		ft::vector<int> foo (3,100);   // three ints with a value of 100
+		ft::vector<int> bar (5,200);   // five ints with a value of 200
+
+		foo.swap(bar);
+
+		std::cout << "foo contains:";
+		for (unsigned i=0; i<foo.size(); i++)
+			std::cout << ' ' << foo[i];
+		std::cout << '\n';
+
+		std::cout << "bar contains:";
+		for (unsigned i=0; i<bar.size(); i++)
+			std::cout << ' ' << bar[i];
+		std::cout << '\n';
 	}
 
 	// CLEAR
 	{
 		std::cout << std::endl;
+		ft::vector<int> myvector;
+		myvector.push_back (100);
+		myvector.push_back (200);
+		myvector.push_back (300);
+
+		std::cout << "myvector contains:";
+		for (unsigned i=0; i<myvector.size(); i++)
+			std::cout << ' ' << myvector[i];
+		std::cout << '\n';
+
+		myvector.clear();
+		myvector.push_back (1101);
+		myvector.push_back (2202);
+
+		std::cout << "myvector contains:";
+		for (unsigned i=0; i<myvector.size(); i++)
+			std::cout << ' ' << myvector[i];
+		std::cout << '\n';
 	}
 
-	// GET_ALLOCATOR
+	// GET_ALLOCATOR // TODO: Сделать!!!
 	{
-		std::cout << std::endl;
+		// std::cout << std::endl;
+		// ft::vector<int> myvector;
+		// int * p;
+		// unsigned int i;
+
+		// // allocate an array with space for 5 elements using vector's allocator:
+		// p = myvector.get_allocator().allocate(5);
+
+		// // construct values in-place on the array:
+		// for (i=0; i<5; i++) myvector.get_allocator().construct(&p[i],i);
+
+		// std::cout << "The allocated array contains:";
+		// for (i=0; i<5; i++) std::cout << ' ' << p[i];
+		// std::cout << '\n';
+
+		// // destroy and deallocate:
+		// for (i=0; i<5; i++) myvector.get_allocator().destroy(&p[i]);
+		// myvector.get_allocator().deallocate(p,5);
 	}
 
 	// OPERATORS
 	{
 		std::cout << std::endl;
+		ft::vector<int> foo (3,100);   // three ints with a value of 100
+		ft::vector<int> bar (2,200);   // two ints with a value of 200
+
+		if (foo==bar) std::cout << "foo and bar are equal\n";
+		if (foo!=bar) std::cout << "foo and bar are not equal\n";
+		if (foo< bar) std::cout << "foo is less than bar\n";
+		if (foo> bar) std::cout << "foo is greater than bar\n";
+		if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+		if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
 	}
 
-	// SWAP
+	// SWAP //TODO: здесь продемонстрировать работу swap
 	{
 		std::cout << std::endl;
+		unsigned int i;
+		std::vector<int> foo (3,100);   // three ints with a value of 100
+		std::vector<int> bar (5,200);   // five ints with a value of 200
+
+		ft::swap(foo, bar);
+
+		std::cout << "foo contains:";
+		for (std::vector<int>::iterator it = foo.begin(); it!=foo.end(); ++it)
+			std::cout << ' ' << *it;
+		std::cout << '\n';
+
+		std::cout << "bar contains:";
+		for (std::vector<int>::iterator it = bar.begin(); it!=bar.end(); ++it)
+			std::cout << ' ' << *it;
+		std::cout << '\n';
 	}
 
 }

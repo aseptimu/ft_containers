6d55a82e409cc5df844eac28fc65e47a3318364f
Animus Septimus <aseptimu@at-q6.msk.21-school.ru>
2022-06-10 13:21:50 +0300

    test added

---
+++ main_check.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "vector.hpp"
+#include <vector>
+#include "stack.hpp"
+#include <stack>
+#include "map.hpp"
+#include <map>
+#include "set.hpp"
+#include <set>
+
+int main(void)
+{
+/*
+
+	explicit vector (const allocator_type& alloc = allocator_type());
+	
+	explicit vector (size_type n, const value_type& val = value_type(),
+					const allocator_type& alloc = allocator_type());
+	
+	template <class InputIterator>
+			vector (InputIterator first, InputIterator last,
+					const allocator_type& alloc = allocator_type());
+	
+	vector (const vector& x);
+*/
+
+  // constructors used in the same order as described above:
+	ft::vector<int> first;                                // empty vector of ints
+	ft::vector<int> second (4,100);                       // four ints with value 100
+	ft::vector<int> third (second.begin(),second.end());  // iterating through second
+	ft::vector<int> fourth (third);
+
+  // the iterator constructor can also be used to construct from arrays:
+	int myints[] = {16,2,77,29};
+	std::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+
+	std::cout << "The contents of fifth are:";
+	for (std::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+}
